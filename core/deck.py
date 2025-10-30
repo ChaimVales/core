ca4832c787092b7504f1030d83d449eed18008d2
Haim Vales <a0534110909@gmail.com>
2025-10-30 12:12:01 +0200

    end test

--- core/deck.py
+++ core/deck.py
@@ -14,9 +14,11 @@ def shuffle_by_suit(deck: list[dict], swaps: int = 5000) -> list[dict]:
     for i in range(swaps):
         index_i = random.randint(0,51)
         index_j = random.randint(0,51)
-        while index_i == index_j or deck[index_i]["suite"] == 'H' and index_j %5 != 0 or deck[index_i]["suite"] == 'C' and index_j %3 != 0 or deck[index_i]["suite"] == 'D' and index_j %2 != 0 or deck[index_i]["suite"] == 'S' and index_j %7 != 0:
+        while (index_i == index_j) or (deck[index_i]["suite"] == 'H' and index_j %5 != 0) or (deck[index_i]["suite"] == 'C' and index_j %3 != 0) or (deck[index_i]["suite"] == 'D' and index_j %2 != 0) or (deck[index_i]["suite"] == 'S' and index_j %7 != 0):
+            print(False,deck[index_i]["suite"],index_j,index_i)
             index_i = random.randint(0,51)
             index_j = random.randint(0,51)
+        print(True,deck[index_i]["suite"],index_j,index_i)
         deck[index_i],deck[index_j] = deck[index_j],deck[index_i]
     return deck
     
